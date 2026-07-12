11e24785a0b3e548e66d1d03f2b987b7e61a792b
Anjali Shelke <anjalishelke1707@gmail.com>
2026-07-12 08:55:22 +0530

    Add files via upload

---
+++ pattern in c/inverted right-angled triangle.c
@@ -0,0 +1,16 @@
+void main()
+{
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(int i = n; i >=1; i--)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ pattern in c/right-angled triangle pattern.c
@@ -0,0 +1,16 @@
+void main()
+{
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
